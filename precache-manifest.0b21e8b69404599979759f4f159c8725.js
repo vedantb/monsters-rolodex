1a290fb04254765f03ecfb09cd876c392e4881aa
Vedant Bhatt <vedantbhatt@oath.com>
2020-04-18 14:23:49 -0700

    Updates

---
+++ precache-manifest.0b21e8b69404599979759f4f159c8725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87c8c2b839a6dce4f54936ca4e652250",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d4e4c9cb4cb82d75dcd3",
+    "url": "/monsters-rolodex/static/css/main.3687faff.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4e4c9cb4cb82d75dcd3",
+    "url": "/monsters-rolodex/static/js/main.ab7259a9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
